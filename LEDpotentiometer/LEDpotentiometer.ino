48d9913681fc40bbddada0c26b5925222acf2679
Max Farley <maxwfarley@live.co.uk>
2017-03-27 15:31:07 +0100

    Add files via upload

---
+++ LEDpotentiometer/LEDpotentiometer.ino
@@ -0,0 +1,51 @@
+int led1 = 2;
+int led2 = 3;
+int led3 = 4;
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = 9; // Analog output pin that the LED is attached to
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);
+ 
+
+  // print the results to the serial monitor:
+
+  Serial.println(sensorValue);
+
+  if(sensorValue > 300){
+    digitalWrite(led1, HIGH);
+  }
+  else{
+    digitalWrite(led1, LOW);
+  }
+
+  if(sensorValue > 600){
+    digitalWrite(led2, HIGH);
+  }
+  else{
+    digitalWrite(led2,LOW);
+  }
+
+  if(sensorValue > 900){
+    digitalWrite(led3, HIGH);
+  }
+  else{
+    digitalWrite(led3, LOW);
+  }
+   
+
+ 
+  // wait 2 milliseconds before the next loop
+  // for the analog-to-digital converter to settle
+  // after the last reading:
+  delay(2);
+}
